ee2f4c873646adb4b6c2f32390c78f49e406c83a
leedavi <dcl@agence-sesame.fr>
2022-07-20 17:33:10 +0200

    admin list

--- API/StartConnect.cs
+++ API/StartConnect.cs
@@ -76,40 +76,67 @@ namespace RocketForms.API
         }
         public string ListForm()
         {
-            var moduleRef = _paramInfo.GetXmlProperty("genxml/remote/moduleref");
-            var portalid = PortalUtils.GetCurrentPortalId();
-            var remoteModule = new RemoteModule(portalid, moduleRef);
-            var appTheme = new AppThemeLimpet(portalid, remoteModule.AppThemeViewFolder, remoteModule.AppThemeViewVersion, remoteModule.Organisation);
-
-            // Get list of forms from Directory
-            var formlist = new List<SimplisityRecord>();
-            var folderPath = PortalUtils.HomeDirectoryMapPath(_portalId) + "\\RocketForms\\" + moduleRef;
-            var l = Directory.GetFiles(folderPath).OrderByDescending(i => i);
-            foreach (var f in l)
+            try
             {
-                var fData = GeneralUtils.Base64Decode(FileUtils.ReadFile(f));
-                var sRec = new SimplisityRecord();
-                var sRecDecode = new SimplisityRecord();
-                sRec.FromXmlItem(fData);
-                var dList = sRec.ToDictionary();
-                foreach (var d in dList)
+
+                var moduleRef = _paramInfo.GetXmlProperty("genxml/remote/moduleref");
+                var portalid = PortalUtils.GetCurrentPortalId();
+                var remoteModule = new RemoteModule(portalid, moduleRef);
+                var appTheme = new AppThemeLimpet(portalid, remoteModule.AppThemeViewFolder, remoteModule.AppThemeViewVersion, remoteModule.Organisation);
+
+                // Get list of forms from Directory
+                var formlist = new List<SimplisityRecord>();
+                var folderPath = PortalUtils.HomeDirectoryMapPath(_portalId) + "\\RocketForms\\" + moduleRef;
+                var l = Directory.GetFiles(folderPath).OrderByDescending(i => i);
+                foreach (var f in l)
                 {
-                    var strText = d.Value;
-                    strText = strText.Replace(Environment.NewLine, "<br/>");
-                    strText = strText.Replace("\n", "<br/>");
-                    strText = strText.Replace("\r", "<br/>");
-                    sRecDecode.SetXmlProperty("genxml/" + d.Key, strText);
+                    var fData = FileUtils.ReadFile(f);
+                    var sRec = new SimplisityRecord();
+                    var sRecDecode = new SimplisityRecord();
+                    sRec.FromXmlItem(fData);
+                    var dList = sRec.ToDictionary();
+                    foreach (var d in dList)
+                    {
+                        var strText = d.Value;
+
+                        if (d.Key == "createddate")
+                        {
+                            sRecDecode.SetXmlProperty("genxml/" + d.Key, strText);
+                        }
+                        else if (d.Key.StartsWith("base64-"))
+                        {
+                            string[] b64split = strText.Split(new[] { '-', ',' });
+                            if (b64split.Length == 2)
+                            {
+                                sRecDecode.SetXmlProperty("genxml/" + d.Key + "meta", b64split[0]);
+                                sRecDecode.SetXmlProperty("genxml/" + d.Key + "value", b64split[1]);
+                            }
+                            sRecDecode.SetXmlProperty("genxml/" + d.Key, strText);
+                        }
+                        else
+                        { 
+                            strText = strText.Replace(Environment.NewLine, "<br/>");
+                            strText = strText.Replace("\n", "<br/>");
+                            strText = strText.Replace("\r", "<br/>");
+                            sRecDecode.SetXmlProperty("genxml/" + d.Key, GeneralUtils.Base64Decode(strText));
+                        }
+                    }
+                    sRec.XMLData = sRecDecode.XMLData;
+                    formlist.Add(sRec);
                 }
-                sRec.XMLData = sRecDecode.XMLData;
-                formlist.Add(sRec);
+                var dataObjects = new Dictionary<string, object>();
+                dataObjects.Add("remotemodule", remoteModule);
+                dataObjects.Add("formlist", formlist);
+                var razorTempl = appTheme.GetTemplate("AdminList.cshtml");
+                var pr = RenderRazorUtils.RazorProcessData(razorTempl, _postInfo, dataObjects, null, _sessionParams, true);
+                if (pr.StatusCode != "00") return pr.ErrorMsg;
+                return pr.RenderedText;
             }
-            var dataObjects = new Dictionary<string, object>();
-            dataObjects.Add("remotemodule", remoteModule);
-            dataObjects.Add("formlist", formlist);
-            var razorTempl = appTheme.GetTemplate("AdminList.cshtml");
-            var pr = RenderRazorUtils.RazorProcessData(razorTempl, _postInfo, dataObjects, null, _sessionParams, true);
-            if (pr.StatusCode != "00") return pr.ErrorMsg;
-            return pr.RenderedText;
+            catch (Exception ex)
+            {
+                LogUtils.LogException(ex);
+                return ex.ToString();
+            }
         }
 
         public string PostForm()
@@ -128,43 +155,58 @@ namespace RocketForms.API
             rtnRecord.SetXmlProperty("genxml/data/createddate", DateTime.Now.ToString("O"),TypeCode.DateTime);
             foreach (var d in dList)
             {
-                var strText = d.Value;
-                strText = System.Web.HttpUtility.HtmlEncode(strText);
-                strText = strText.Replace("&gt;", "-");
-                strText = strText.Replace("&lt;", "-");
-                rtnRecord.SetXmlProperty("genxml/data/" + d.Key, strText); 
+                if (d.Key.StartsWith("base64-"))
+                {
+                    rtnRecord.SetXmlProperty("genxml/data/" + d.Key, d.Value.Replace(" ", "+"));
+                }
+                else
+                {
+                    var strText = d.Value;
+                    strText = System.Web.HttpUtility.HtmlEncode(strText);
+                    strText = strText.Replace("&gt;", "-");
+                    strText = strText.Replace("&lt;", "-");
+                    rtnRecord.SetXmlProperty("genxml/data/" + d.Key, GeneralUtils.Base64Encode(strText));
+                }
             }
             var filename = DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss").Replace(" ", "") + "_" + GeneralUtils.GetGuidKey();
             rtnRecord.GUIDKey = filename;
-            var dataSave = GeneralUtils.Base64Encode(rtnRecord.ToXmlItem());
-            FileUtils.SaveFile(folderPath + "\\" + filename, dataSave);
+            var dataSave = rtnRecord.ToXmlItem();
 
-            var portalContent = new PortalContentLimpet(portalid, _sessionParams.CultureCodeEdit); // Portal 0 is admin, editing portal setup
+            var recordByteSize = System.Text.Encoding.Unicode.GetByteCount(dataSave);
 
-            var dataObjects = new Dictionary<string, object>();
-            dataObjects.Add("remotemodule", remoteModule);
-            dataObjects.Add("portalcontent", portalContent);
-
-            var razorTemplEmail = appTheme.GetTemplate("EmailForm.cshtml");
-            var pr = RenderRazorUtils.RazorProcessData(razorTemplEmail, new SimplisityInfo(rtnRecord), dataObjects, null, _sessionParams, true);
-            if (pr.StatusCode != "00") LogUtils.LogSystem("ERROR - RocketForms Email: " + pr.ErrorMsg);
-            if (portalContent.DebugMode)
+            if (recordByteSize < 50000000) // 50MB limit
             {
-                var emailfilename = PortalUtils.TempDirectoryMapPath() + "\\rocketforms_email.html";
-                if (pr.StatusCode != "00")
-                    FileUtils.SaveFile(emailfilename, pr.ErrorMsg);
-                else
-                    FileUtils.SaveFile(emailfilename, pr.RenderedText);
+
+                FileUtils.SaveFile(folderPath + "\\" + filename, dataSave);
+
+                var portalContent = new PortalContentLimpet(portalid, _sessionParams.CultureCodeEdit); // Portal 0 is admin, editing portal setup
+
+                var dataObjects = new Dictionary<string, object>();
+                dataObjects.Add("remotemodule", remoteModule);
+                dataObjects.Add("portalcontent", portalContent);
+
+                var razorTemplEmail = appTheme.GetTemplate("EmailForm.cshtml");
+                var pr = RenderRazorUtils.RazorProcessData(razorTemplEmail, new SimplisityInfo(rtnRecord), dataObjects, null, _sessionParams, true);
+                if (pr.StatusCode != "00") LogUtils.LogSystem("ERROR - RocketForms Email: " + pr.ErrorMsg);
+                if (portalContent.DebugMode)
+                {
+                    var emailfilename = PortalUtils.TempDirectoryMapPath() + "\\rocketforms_email.html";
+                    if (pr.StatusCode != "00")
+                        FileUtils.SaveFile(emailfilename, pr.ErrorMsg);
+                    else
+                        FileUtils.SaveFile(emailfilename, pr.RenderedText);
+                }
+                if (portalContent.EmailOn && pr.StatusCode == "00" && remoteModule.Record.GetXmlPropertyBool("genxml/checkbox/emailon"))
+                {
+                    var eFunc = new EmailLimpet(portalid, _sessionParams.CultureCode);
+                    eFunc.SendEmail(pr.RenderedText, remoteModule.Record.GetXmlProperty("genxml/textbox/fromemail"), rtnRecord.GetXmlProperty("genxml/data/email"), remoteModule.Record.GetXmlProperty("genxml/textbox/replytoemail"), remoteModule.Record.GetXmlProperty("genxml/textbox/subject"));
+                }
+                var razorTempl = appTheme.GetTemplate("SentMessage.cshtml");
+                pr = RenderRazorUtils.RazorProcessData(razorTempl, _postInfo, dataObjects, null, _sessionParams, true);
+                if (pr.StatusCode != "00") return pr.ErrorMsg;
+                return pr.RenderedText;
             }
-            if (portalContent.EmailOn && pr.StatusCode == "00" && remoteModule.Record.GetXmlPropertyBool("genxml/checkbox/emailon"))
-            {
-                var eFunc = new EmailLimpet(portalid, _sessionParams.CultureCode);
-                eFunc.SendEmail(pr.RenderedText, remoteModule.Record.GetXmlProperty("genxml/textbox/fromemail"), rtnRecord.GetXmlProperty("genxml/data/email"), remoteModule.Record.GetXmlProperty("genxml/textbox/replytoemail"), remoteModule.Record.GetXmlProperty("genxml/textbox/subject"));
-            }
-            var razorTempl = appTheme.GetTemplate("SentMessage.cshtml");
-            pr = RenderRazorUtils.RazorProcessData(razorTempl, _postInfo, dataObjects, null, _sessionParams, true);
-            if (pr.StatusCode != "00") return pr.ErrorMsg;
-            return pr.RenderedText;
+            return "Invalid record";
         }
     }
 
